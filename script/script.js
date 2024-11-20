d41d8eac692ca725b564993a9ecbf456253cdf9c
CarloDettori <dettoric07@gmail.com>
2024-11-20 12:10:51 +0100

    img from server in polaroids + starting for overlay building

--- script/script.js
+++ script/script.js
@@ -6,11 +6,10 @@ let polaroidBox = document.getElementById("polaroid-box");
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((ans) => {
     let deck = ""
     for (let i = 0; i <= 5; i++) {
-        console.log(ans.data[i].title)
         deck += `
-        <div id="polaroid-1" class="polaroid">
+        <div id="polaroid-${ans.data[i].id}" class="polaroid">
                     <img id="pin" src="./img/pin.svg" alt="">
-                    <img class="debug" id="" src="./img/aspect-ratios-blogpost-1x1-1.png" alt="image">
+                    <img class="debug" id="" src="${ans.data[i].thumbnailUrl}" alt="image">
                     <p class="debug">${ans.data[i].title}</p>
                 </div>
                 `
@@ -20,5 +19,4 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((ans) => 
     .catch((error) => {
         console.log(error);
     })
-
 
