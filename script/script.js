5b8f5e8cd1e80a1f9f32f85137c33976be10f80a
CarloDettori <dettoric07@gmail.com>
2024-11-20 09:46:48 +0100

    rendering optimization for inner html in card box

--- script/script.js
+++ script/script.js
@@ -4,9 +4,10 @@ console.clear()
 let polaroidBox = document.getElementById("polaroid-box");
 
 axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((ans) => {
+    let deck = ""
     for (let i = 0; i <= 5; i++) {
         console.log(ans.data[i].title)
-        polaroidBox.innerHTML += `
+        deck += `
         <div id="polaroid-1" class="polaroid">
                     <img id="pin" src="./img/pin.svg" alt="">
                     <img class="debug" id="" src="./img/aspect-ratios-blogpost-1x1-1.png" alt="image">
@@ -14,6 +15,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then((ans) => 
                 </div>
                 `
     }
+    polaroidBox.innerHTML = deck;
 })
     .catch((error) => {
         console.log(error);
