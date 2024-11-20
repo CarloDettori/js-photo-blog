bfe03f211be50bcf3312b06423022619c7f4bd23
CarloDettori <dettoric07@gmail.com>
2024-11-20 22:16:46 +0100

    mi arrendo

--- script/script.js
+++ script/script.js
@@ -11,8 +11,8 @@ axios
         for (let i = 0; i <= 5; i++) {
             deck += `
                 <div id="polaroid-${ans.data[i].id}" class="polaroid">
-                    <img id="pin" src="./img/pin.svg" alt="">
-                    <img class="debug" id="${ans.data[i].id}" src="${ans.data[i].thumbnailUrl}" alt="image">
+                    <img  id="pin" src="./img/pin.svg" alt="">
+                    <img class="polaroid-img" id="${ans.data[i].id}" src="${ans.data[i].thumbnailUrl}" alt="image">
                     <p class="debug">${ans.data[i].title}</p>
                 </div>
             `;
@@ -29,14 +29,16 @@ let closeButton = document.getElementById("close-button");
 let polaroidOverlay = document.getElementById("zoom-image")
 let overlay = document.getElementById("overlay")
 
-function showOverlay() {
+polaroid.forEach(addEventListener("click", function (element) {
+    let ThisPolaroid = element.currentTarget;
+    //let ThisPolaroidImg = ThisPolaroid.getElementsByClassName("polaroid-img");
+    //polaroidOverlay.src = ThisPolaroidImg.src;
     overlay.classList.remove("d-none");
-}
-polaroid.forEach(addEventListener("click", showOverlay));
+    console.log(ThisPolaroid)
+}))
 
 function hideOverlay() {
-    //let ThisPolaroidImg = polaroid.querySelector("img !#pin")
-    //polaroidOverlay.src = ThisPolaroidImg.src;
+
     overlay.classList.add("d-none");
 }
 closeButton.addEventListener("click", hideOverlay);
