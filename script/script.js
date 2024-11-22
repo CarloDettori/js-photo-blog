089fe107ee4cea3bcd4e6d7069d04b83083bd774
CarloDettori <dettoric07@gmail.com>
2024-11-22 10:09:50 +0100

    adding photo to overlay

--- script/script.js
+++ script/script.js
@@ -2,14 +2,15 @@
 console.clear()
 
 let polaroidBox = document.getElementById("polaroid-box");
-
+let closeButton = document.getElementById("close-button");
+let polaroidOverlay = document.getElementById("zoom-image")
 axios
     .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then((ans) => {
         let deck = ""
         console.log(ans.data)
         document.getElementById("zoom-image")
-        for (let i = 0; i <= ans.data.length; i++) {
+        for (let i = 0; i < ans.data.length; i++) {
             deck += `
                 <div id="polaroid-${ans.data[i].id}" class="polaroid">
                     <img  id="pin" src="./img/pin.svg" alt="">
@@ -22,9 +23,15 @@ axios
         let polaroid = document.getElementsByClassName("polaroid")
 
         let overlay = document.getElementById("overlay")
-        console.log(polaroid)
+        //console.log(polaroid)
         Array.from(polaroid).forEach((value) => {
-            value.addEventListener("click",)
+            value.addEventListener("click", (Event) => {
+                let ThisPolaroidImgUrl = Event.currentTarget.querySelector(":nth-child(2)").src;
+                console.log(ThisPolaroidImgUrl);
+                polaroidOverlay.src = ThisPolaroidImgUrl
+                overlay.classList.remove("d-none");
+
+            })
         })
     })
     .catch((error) => {
@@ -33,8 +40,6 @@ axios
     })
 
 
-let closeButton = document.getElementById("close-button");
-let polaroidOverlay = document.getElementById("zoom-image")
 
 function hideOverlay() {
 
