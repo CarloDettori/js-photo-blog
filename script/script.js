4498dea4ae2a552c8f656495ff1be7e45ed262f3
CarloDettori <dettoric07@gmail.com>
2024-11-24 16:43:57 +0100

    some fix

--- script/script.js
+++ script/script.js
@@ -8,13 +8,13 @@ axios
     .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then((ans) => {
         let deck = ""
-        console.log(ans.data)
+        //console.log(ans.data)
         document.getElementById("zoom-image")
         for (let i = 0; i < ans.data.length; i++) {
             deck += `
                 <div id="polaroid-${ans.data[i].id}" class="polaroid">
                     <img  id="pin" src="./img/pin.svg" alt="">
-                    <img class="polaroid-img" id="${ans.data[i].id}" src="${ans.data[i].thumbnailUrl}" alt="image">
+                    <img class="polaroid-img" id="${ans.data[i].id}" src="${ans.data[i].url}" alt="image">
                     <p class="debug">${ans.data[i].title}</p>
                 </div>
             `;
